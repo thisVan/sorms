e87b05e4aa22fd079a55e1e13231e6bb67a40eec
bolven <bolven@126.com>
2018-02-08 11:42:27 +0800

    #改造为Maven项目

--- src/java/com/nfledmedia/sorm/util/List2Json.java
+++ src/java/com/nfledmedia/sorm/util/List2Json.java
@@ -8,20 +8,18 @@ import net.sf.json.JSONException;
 import net.sf.json.JSONObject;
 
 /**
- * 项目名称：sorm 类全名:com.nfledmedia.sorm.util.List2Json 类描述：
- * 创建人：Van@nfledmedia 创建时间：2016年6月16日 下午7:20:37 修改备注：
+ * 项目名称：sorm 类全名:com.nfledmedia.sorm.util.List2Json 类描述： 创建人：Van@nfledmedia
+ * 创建时间：2016年6月16日 下午7:20:37 修改备注：
  * 
  * @version jdk1.7
  * 
- * Copyright (c) 2016, bolven@qq.com All Rights Reserved.
+ *          Copyright (c) 2016, bolven@qq.com All Rights Reserved.
  */
 public class List2Json {
-	public static JSONObject listToJSON(List list) throws JSONException,
-			SQLException {
+	public static JSONObject listToJSON(List list) throws JSONException, SQLException {
 		JSONObject list_json = new JSONObject();
 		try {
-			for (Iterator<Throwable> iterator = ((SQLException) list)
-					.iterator(); iterator.hasNext();) {
+			for (Iterator<Throwable> iterator = ((SQLException) list).iterator(); iterator.hasNext();) {
 				Object obj = iterator.next();
 				// list_json.put("JobID", obj.getJobID());
 				// list_json.put("AssistantNO", obj.getAssistantNO());
